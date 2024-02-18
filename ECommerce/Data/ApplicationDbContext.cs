68074ce201acc333f4148ca0d32c0826638152ef
Sylvester Ogoe <Sylvesterogoe@gmail.com>
2024-02-18 13:10:33 +0000

    Code cleanup again

--- ECommerce/Data/ApplicationDbContext.cs
+++ ECommerce/Data/ApplicationDbContext.cs
@@ -14,8 +14,4 @@ public class ApplicationDbContext : DbContext
     public DbSet<Item> Items { get; set; }
     public DbSet<Cart> Carts { get; set; }
     public DbSet<CartItem> CartItems { get; set; }
-
-    protected override void OnModelCreating(ModelBuilder modelBuilder)
-    {
-    }
 }
